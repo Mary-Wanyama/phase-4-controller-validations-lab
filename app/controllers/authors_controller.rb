d63698036b1e97e7f328ca23603792c14303ea64
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-11-25 18:44:37 +0300

    Completed assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,8 @@
+
 class AuthorsController < ApplicationController
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
   
   def show
     author = Author.find(params[:id])
@@ -7,7 +11,7 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
+    author = Author.create!(author_params)
 
     render json: author, status: :created
   end
@@ -15,7 +19,12 @@ class AuthorsController < ApplicationController
   private
   
   def author_params
+    
     params.permit(:email, :name)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
   
 end
